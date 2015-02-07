81f43a950fec940194a6f90d293aabb04a894287
lohkii69 <jmckay2@hotmail.com>
2015-02-07 12:02:27 -0700

    Card Chapter 8

---
+++ Card Chapter 8/Card Chapter 8/Card.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_Chapter_8
+{
+    class Card
+    {
+        public suits Suit;
+        public values Value;
+
+        public string Name { get { return this.Value + " of " + this.Suit; } }
+
+        public enum suits
+        {
+            Spades = 0,
+            Clubs = 1,
+            Diamonds = 2,
+            Hearts = 3,
+        }
+
+        public enum values
+        {
+            Ace = 1,
+            Two = 2,
+            Three = 3,
+            Four = 4,
+            Five = 5,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            Ten = 10,
+            Jack = 11,
+            Queen = 12,
+            King = 13,
+        }
+
+        public Card(suits suit, values value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+    }
+}
---
+++ Card Chapter 8/Card Chapter 8/CardForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Card_Chapter_8
+{
+    public partial class CardForm : Form
+    {
+        public CardForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCard_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            Card card = new Card((Card.suits)random.Next(4), (Card.values)random.Next(1, 14));
+            MessageBox.Show(card.Name);
+        }
+    }
+}
