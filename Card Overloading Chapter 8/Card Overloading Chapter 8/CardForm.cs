0b5fb67bf70ec5e49bf476a47a1a931f2a39aadf
lohkii69 <jmckay2@hotmail.com>
2015-02-07 11:59:04 -0700

    Card Overloading Chapter 8

---
+++ Card Overloading Chapter 8/Card Overloading Chapter 8/CardForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Card_Overloading_Chapter_8
+{
+    public partial class CardForm : Form
+    {
+        public CardForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Card cardToCheck = new Card(Card.suits.Clubs, Card.values.Three);
+            bool doesItMatch = Card.DoesCardMatch(cardToCheck, Card.suits.Clubs);
+            MessageBox.Show(doesItMatch.ToString());
+
+        }
+    }
+}
