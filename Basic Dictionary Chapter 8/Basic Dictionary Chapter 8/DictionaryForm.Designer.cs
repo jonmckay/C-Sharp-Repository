941fb12e618c5acd54759ec12fd1637129ca52c6
lohkii69 <jmckay2@hotmail.com>
2015-02-07 12:00:00 -0700

    Basic Dictionar Chapter 8

---
+++ Basic Dictionary Chapter 8/Basic Dictionary Chapter 8/DictionaryForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Basic_Dictionary_Chapter_8
+{
+    partial class DictionaryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbNumber = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPlayer = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblYearRetired = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number";
+            // 
+            // cbNumber
+            // 
+            this.cbNumber.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbNumber.FormattingEnabled = true;
+            this.cbNumber.Location = new System.Drawing.Point(72, 17);
+            this.cbNumber.Name = "cbNumber";
+            this.cbNumber.Size = new System.Drawing.Size(48, 21);
+            this.cbNumber.TabIndex = 1;
+            this.cbNumber.SelectedIndexChanged += new System.EventHandler(this.cbNumber_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "was worn by";
+            // 
+            // lblPlayer
+            // 
+            this.lblPlayer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPlayer.Location = new System.Drawing.Point(211, 18);
+            this.lblPlayer.Name = "lblPlayer";
+            this.lblPlayer.Size = new System.Drawing.Size(88, 20);
+            this.lblPlayer.TabIndex = 3;
+            this.lblPlayer.Text = "Jackie Robinson";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(305, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "and retired in";
+            // 
+            // lblYearRetired
+            // 
+            this.lblYearRetired.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblYearRetired.Location = new System.Drawing.Point(379, 18);
+            this.lblYearRetired.Name = "lblYearRetired";
+            this.lblYearRetired.Size = new System.Drawing.Size(56, 20);
+            this.lblYearRetired.TabIndex = 5;
+            this.lblYearRetired.Text = "1993";
+            // 
+            // DictionaryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 52);
+            this.Controls.Add(this.lblYearRetired);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblPlayer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "DictionaryForm";
+            this.Text = "Retired Jersery Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPlayer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblYearRetired;
+    }
+}
+
