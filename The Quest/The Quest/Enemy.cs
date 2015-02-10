7443e6d32ca9848eea67bf7fda8bd659a78b80e5
lohkii69 <jmckay2@hotmail.com>
2015-02-10 16:34:04 -0700

    The Quest enemy file

---
+++ The Quest/The Quest/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Quest
+{
+    abstract class Enemy : Mover
+    {
+        internal override void Move(Random random)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
