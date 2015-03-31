1c9eb1c1e7b3c2e527afc0aeecf5a1ca15896d01
lohkii69 <jmckay2@hotmail.com>
2015-03-31 16:35:02 -0600

    Added bat class

---
+++ Event Handlers Chapter 11/Event Handlers Chapter 11/Bat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Handlers_Chapter_11
+{
+    class Bat
+    {
+        delegate void BatCallback(BallEventArgs e);
+
+        private BatCallback hitBallCallback;
+        public Bat(BatCallback callbackDelegate)
+        {
+            this.hitBallCallback = new BatCallback(callbackDelegate);
+        }
+
+        public void HitTheBall(BallEventArgs e)
+        {
+            if (hitBallCallback != null)
+            {
+                hitBallCallback(e);
+            }
+        }
+    }
+}
