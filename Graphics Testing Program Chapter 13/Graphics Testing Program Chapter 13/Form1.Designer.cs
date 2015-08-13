fa9dc204bb519f902c5b6bd198be36b8167309c8
lohkii69 <jmckay2@hotmail.com>
2015-08-13 11:07:37 -0600

    Used paint event to correct the redrawing of images onto the form

--- Graphics Testing Program Chapter 13/Graphics Testing Program Chapter 13/Form1.Designer.cs
+++ Graphics Testing Program Chapter 13/Graphics Testing Program Chapter 13/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Click += new System.EventHandler(this.Form1_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
 
         }
--- Graphics Testing Program Chapter 13/Graphics Testing Program Chapter 13/Form1.cs
+++ Graphics Testing Program Chapter 13/Graphics Testing Program Chapter 13/Form1.cs
@@ -31,26 +31,53 @@ namespace Graphics_Testing_Program_Chapter_13
             InitializeComponent();
         }
 
+        public void DrawBee(Graphics g, Rectangle rect)
+        {
+            g.DrawImage(Properties.Resources.Bee_animation_1, rect);
+        }
+
         private void Form1_Click(object sender, EventArgs e)
         {
-            using (Graphics g = this.CreateGraphics())
+            using (Graphics g = CreateGraphics())
             {
-                g.FillRectangle(Brushes.SkyBlue, ClientRectangle);
-                g.DrawImage(Properties.Resources.Bee_animation_1, 50, 20, 75, 75);
-                g.DrawImage(Properties.Resources.Flower, 10, 130, 100, 150);
-
-                using (Pen thickBlackPen = new Pen(Brushes.Black, 3.0F)) 
-                {
-                    g.DrawLines(thickBlackPen, new Point[] { new Point(130, 110), new Point(120, 160), new Point(155, 163) });
-                    g.DrawCurve(thickBlackPen, new Point[] { new Point(120, 160), new Point(175, 120), new Point(215, 70) });
-                }
-
-                using (Font font = new Font("Arial", 16, FontStyle.Italic))
-                {
-                    SizeF size = g.MeasureString("Nectar here", font);
-                    g.DrawString("Nectar here", font, Brushes.Red, new Point(215 - (int)size.Width / 2, 70 - (int)size.Height));
-                }
+                g.DrawImage(Properties.Resources.Hive__inside_, -Width, -Height, Width * 2, Height * 2);
+                Size size = new Size(Width / 5, Height / 5);
+                DrawBee(g, new Rectangle(new Point(Width / 2 - 50, Height / 2 - 40), size));
+                DrawBee(g, new Rectangle(new Point(Width / 2 - 20, Height / 2 - 60), size));
+                DrawBee(g, new Rectangle(new Point(Width / 2 - 80, Height / 2 - 30), size));
+                DrawBee(g, new Rectangle(new Point(Width / 2 - 90, Height / 2 - 80), size));
             }
+
+
+            //using (Graphics g = this.CreateGraphics())
+            //{
+            //    g.FillRectangle(Brushes.SkyBlue, ClientRectangle);
+            //    g.DrawImage(Properties.Resources.Bee_animation_1, 50, 20, 75, 75);
+            //    g.DrawImage(Properties.Resources.Flower, 10, 130, 100, 150);
+
+            //    using (Pen thickBlackPen = new Pen(Brushes.Black, 3.0F)) 
+            //    {
+            //        g.DrawLines(thickBlackPen, new Point[] { new Point(130, 110), new Point(120, 160), new Point(155, 163) });
+            //        g.DrawCurve(thickBlackPen, new Point[] { new Point(120, 160), new Point(175, 120), new Point(215, 70) });
+            //    }
+
+            //    using (Font font = new Font("Arial", 16, FontStyle.Italic))
+            //    {
+            //        SizeF size = g.MeasureString("Nectar here", font);
+            //        g.DrawString("Nectar here", font, Brushes.Red, new Point(215 - (int)size.Width / 2, 70 - (int)size.Height));
+            //    }
+            //}
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(Properties.Resources.Hive__inside_, -Width, -Height, Width * 2, Height * 2);
+            Size size = new Size(Width / 5, Height / 5);
+            DrawBee(g, new Rectangle(new Point(Width / 2 - 50, Height / 2 - 40), size));
+            DrawBee(g, new Rectangle(new Point(Width / 2 - 20, Height / 2 - 60), size));
+            DrawBee(g, new Rectangle(new Point(Width / 2 - 80, Height / 2 - 30), size));
+            DrawBee(g, new Rectangle(new Point(Width / 2 - 90, Height / 2 - 80), size));
         }
 
 
