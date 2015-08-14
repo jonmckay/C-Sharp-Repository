a80c6652c57bbd5827e42a1aee12814025a20e55
lohkii69 <jmckay2@hotmail.com>
2015-08-14 15:47:53 -0600

    Printing Application first commit

---
+++ Printing Application Chapter 13/Printing Application Chapter 13/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+
+namespace Printing_Application_Chapter_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PrintDocument document = new PrintDocument();
+            document.PrintPage += document_PrintPage;
+            document.Print();
+        }
+
+        void document_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
