8dbc29fcc5d692c1e71afed34d580d988b18eef5
lohkii69 <jmckay2@hotmail.com>
2015-08-21 14:13:55 -0600

    Added Garbage collection application

---
+++ Garbage Collection Chapter 14/Garbage Collection Chapter 14/Clone.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Garbage_Collection_Chapter_14
+{
+    class Clone : IDisposable
+    {
+        public int Id { get; private set; }
+
+        public Clone(int Id)
+        {
+            this.Id = Id;
+        }
+
+        public void Dispose()
+        {
+            MessageBox.Show("I've been disposed!", "Clone #" + Id + " says...");
+        }
+
+        ~Clone()
+        {
+            MessageBox.Show("Aaargh! You got me!", "Clone #" + Id + " says...");
+        }
+    }
+}
---
+++ Garbage Collection Chapter 14/Garbage Collection Chapter 14/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Garbage_Collection_Chapter_14
+{
+    public partial class CloneForm : Form
+    {
+        public CloneForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCloneOne_Click(object sender, EventArgs e)
+        {
+            using (Clone clone1 = new Clone(1))
+            {
+                // Do nothing!
+            }
+        }
+
+        private void btnCloneTwo_Click(object sender, EventArgs e)
+        {
+            Clone clone2 = new Clone(2);
+            clone2 = null;
+        }
+
+        private void btnGC_Click(object sender, EventArgs e)
+        {
+            GC.Collect();
+        }
+    }
+}
