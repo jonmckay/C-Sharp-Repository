8370ba94e1a778cc21bc34255a9835567b546768
lohkii69 <jmckay2@hotmail.com>
2015-08-25 15:59:36 -0600

    Started structure activity from head first c#

---
+++ Structs Chapter 14/Structs Chapter 14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs_Chapter_14
+{
+    class Program
+    {
+        public struct Dog
+        {
+            public string Name;
+            public string Breed;
+
+            public Dog(string name, string breed)
+            {
+                this.Name = name;
+                this.Breed = breed;
+            }
+
+            public void Speak()
+            {
+                Console.WriteLine("My name is {0} and I'm a {1}.", Name, Breed);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
