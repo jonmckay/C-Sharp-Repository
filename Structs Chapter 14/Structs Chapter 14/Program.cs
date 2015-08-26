e0d1767ea1220df30ca3009cf860cc7ce1be9e1d
lohkii69 <jmckay2@hotmail.com>
2015-08-26 09:56:36 -0600

    Added code to the main method in order to compare classes and structures

--- Structs Chapter 14/Structs Chapter 14/Program.cs
+++ Structs Chapter 14/Structs Chapter 14/Program.cs
@@ -27,6 +27,19 @@ namespace Structs_Chapter_14
 
         static void Main(string[] args)
         {
+            Canine spot = new Canine("Spot", "pug");
+            Canine bob = spot;
+            bob.Name = "spike";
+            bob.Breed = "beagle";
+            spot.Speak();
+
+            Dog jake = new Dog("Jake", "poodle");
+            Dog betty = jake;
+            betty.Name = "Betty";
+            betty.Breed = "pit bull";
+            jake.Speak();
+
+            Console.ReadKey();
         }
     }
 }
