27f77ca868952ea857a3d48c7ea5f8b801c69083
lohkii69 <jmckay2@hotmail.com>
2015-08-26 15:53:33 -0600

    Making variables robust with question marks and null

---
+++ RobustGuy Chapter 14/RobustGuy Chapter 14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobustGuy_Chapter_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter birthday: ");
+            string birthday = Console.ReadLine();
+            Console.Write("Enter heigh in inches: ");
+            string height = Console.ReadLine();
+            RobustGuy guy = new RobustGuy(birthday, height);
+            Console.WriteLine(guy.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RobustGuy Chapter 14/RobustGuy Chapter 14/RobustGuy.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobustGuy_Chapter_14
+{
+    class RobustGuy
+    {
+        public DateTime? Birthday { get; private set; }
+        public int? Height { get; private set; }
+
+        public RobustGuy(string birthday, string height)
+        {
+            DateTime tempDate;
+            if (DateTime.TryParse(birthday, out tempDate))
+            {
+                Birthday = tempDate;
+            }
+            else
+            {
+                Birthday = null;
+            }
+            int tempInt;
+            if (int.TryParse(height, out tempInt))
+            {
+                Height = tempInt;
+            }
+            else
+            {
+                Height = null;
+            }
+        }
+
+        public override string ToString()
+        {
+            string description;
+            if (Birthday != null)
+            {
+                description = "I was born on " + Birthday.Value.ToLongDateString();
+            }
+            else
+            {
+                description = "I don't know my birthday";
+            }
+            if (Height != null)
+            {
+                description += ", and I'm " + Height + " inches tall";
+            }
+            else
+            {
+                description += ", and I don't know my height";
+            }
+            return description;
+        }
+
+    }
+}
