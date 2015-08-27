ecee1d68dbb5e45a2fc9dec1d9b6cd034cd9de2d
lohkii69 <jmckay2@hotmail.com>
2015-08-27 10:19:00 -0600

    Example Projects using extension of strings and classes

---
+++ Extension Example Chapter 14/Extension Example Chapter 14/OrdinaryHuman.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Example_Chapter_14
+{
+    sealed class OrdinaryHuman
+    {
+        private int age;
+        int weight;
+
+        public OrdinaryHuman(int weight)
+        {
+            this.weight = weight;
+        }
+
+        public void GoToWork() { }
+        public void PayBills() { }
+    }
+}
