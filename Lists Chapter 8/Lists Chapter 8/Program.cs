ab8a06fcaf37bcbac26113f8c0f0631027be1dc3
Jon <jmckay2@hotmail.com>
2015-08-29 18:54:45 -0600

    Committing chapter 8 lists

---
+++ Lists Chapter 8/Lists Chapter 8/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists_Chapter_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shoe> shoeCloset = new List<Shoe>();
+
+            shoeCloset.Add(new Shoe() { style = Shoe.Style.Sneakers, Color = "Black" });
+
+            shoeCloset.Add(new Shoe() { style = Shoe.Style.Clogs, Color = "Brown" });
+
+            shoeCloset.Add(new Shoe() { style = Shoe.Style.Wingtips, Color = "Black" });
+
+            shoeCloset.Add(new Shoe() { style = Shoe.Style.Loafers, Color = "White" });
+
+            shoeCloset.Add(new Shoe() { style = Shoe.Style.Loafers, Color = "Red" });
+
+            shoeCloset.Add(new Shoe() { style = Shoe.Style.Sneakers, Color = "Green" });
+
+            int numberOfShoes = shoeCloset.Count;
+            foreach (Shoe shoe in shoeCloset)
+            {
+                shoe.style = Shoe.Style.Flipflops;
+                shoe.Color = "Orange";
+            }
+
+            shoeCloset.RemoveAt(4);
+
+            Shoe thirdShoe = shoeCloset[3];
+            Shoe secondShoe = shoeCloset[2];
+            shoeCloset.Clear();
+
+            shoeCloset.Add(thirdShoe);
+            if (shoeCloset.Contains(secondShoe))
+            {
+                Console.WriteLine("That's surprising.");
+            }
+
+        }
+    }
+}
---
+++ Lists Chapter 8/Lists Chapter 8/Shoe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lists_Chapter_8
+{
+    class Shoe
+    {
+        public Style style;
+        public string Color;
+
+        public enum Style
+        {
+            Sneakers,
+            Loafers,
+            Sandals,
+            Flipflops,
+            Wingtips,
+            Clogs,
+        }
+    }
+}
