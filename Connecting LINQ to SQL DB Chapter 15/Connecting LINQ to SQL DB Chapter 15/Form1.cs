90a4c6f6fdf5b679767f59c3db3d5615c6118509
Jon <jmckay2@hotmail.com>
2015-08-30 23:12:47 -0600

    Started connecting LINQ to a SQL database activity

---
+++ Connecting LINQ to SQL DB Chapter 15/Connecting LINQ to SQL DB Chapter 15/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connecting_LINQ_to_SQL_DB_Chapter_15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Connecting LINQ to SQL DB Chapter 15/Connecting LINQ to SQL DB Chapter 15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connecting_LINQ_to_SQL_DB_Chapter_15
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
